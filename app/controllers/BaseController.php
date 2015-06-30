c037a800e9a0821ffeb2d03f3d37cb5b48ec7502
ZhiyangLee <zhiyangleecn@gmail.com>
2015-06-30 16:34:54 +0800

    Phalcon基本框架

---
+++ app/controllers/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IT
+ * Date: 2015/6/30
+ * Time: 16:11
+ */
+use Phalcon\Mvc\Controller;
+
+abstract class BaseController extends Controller {
+
+
+
+}
---
+++ app/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: ZhiyangLee
+ * Date: 2015/6/30
+ * Time: 16:09
+ */
+
+class IndexController extends BaseController {
+
+    public function IndexAction()
+    {
+        echo "<h1>Hello!</h1>";
+    }
+
+}
