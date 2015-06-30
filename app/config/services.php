63505223ec3ec1f1c62263e4994cea26312fc4c8
ZhiyangLee <zhiyangleecn@gmail.com>
2015-06-30 16:08:53 +0800

    Phalcon基本框架

--- app/config/services.php
+++ app/config/services.php
@@ -7,6 +7,7 @@
 
 use Phalcon\Mvc\Url as UrlProvider;
 use Phalcon\Session\Adapter\Files as Session;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
 
 /**
  * The URL component is used to generate all kind of urls in the application
@@ -22,4 +23,14 @@ $di->set('session', function() {
     $session = new Session();
     $session->start();
     return $session;
+});
+
+// Database connection is created based on parameters defined in the configuration file
+$di->set('db', function() use ($config) {
+    return new DbAdapter(array(
+        "host"     => $config->database->host,
+        "username" => $config->database->username,
+        "password" => $config->database->password,
+        "dbname"   => $config->database->name
+    ));
 });
